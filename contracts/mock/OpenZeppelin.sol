3934d685e8c8e413798605419032c6b51e20218e
TepNik <tepelin.nikita@gmail.com>
2025-05-14 14:17:58 +0400

    Updated NPM packages

--- contracts/mock/OpenZeppelin.sol
+++ contracts/mock/OpenZeppelin.sol
@@ -3,5 +3,7 @@
 pragma solidity 0.8.30;
 
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {
+    TransparentUpgradeableProxy
+} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
